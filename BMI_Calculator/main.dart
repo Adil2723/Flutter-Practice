2c6f9d5ddb174279d7dc4438399d1d2d5ab8b16b
Muhammad Adil Saeed <masadil942@gmail.com>
2026-07-29 20:58:32 +0500

    Initialize BMI Calculator Flutter app structure

---
+++ BMI_Calculator/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'pages/input_page.dart';
+import 'pages/result_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "BMI Calculation App",
+
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+
+      initialRoute: "/",
+
+      routes: {
+        "/": (context) => const InputPage(),
+        "/bmi": (context) => const ResultPage(),
+      }
+    );
+  }
+  }
